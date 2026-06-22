3390537de641f4e48536ae988e4bc4a9ba40f411
dhanuuuu7 <dhanusuji47@gmail.com>
2026-06-22 22:52:07 +0530

    Implement day check using switch statement

---
+++ WorkTime.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class WorkTime{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String day =  sc.nextLine();
+
+//        if(day.equals("Monday")){
+//            System.out.println("It is a weekday");
+//        }
+//        else if(day.equals("Tuesday")){
+//            System.out.println("It is a weekday");
+//        }
+//        else if(day.equals("Wednwsday")){
+//            System.out.println("It is a weekday");
+//        }
+//        else if(day.equals("Thursday")){
+//            System.out.println("It is a weekday");
+//        }
+//        else if(day.equals("Friday")){
+//            System.out.println("It is a weekday");
+//        }
+//        else if(day.equals("Saturday")){
+//            System.out.println("It is a weekday");
+//        }
+//        else if(day.equals("Sunday")){
+//            System.out.println("It is a weekend");
+//        }
+//        else{
+//            System.out.println(day + " is not a day ");
+//        }
+
+        switch(day){
+            case "Monday "-> System.out.println("It is a weekday 😣");
+            case "Tuesday "-> System.out.println("It is a weekday 😣");
+            case "Wednesday "-> System.out.println("It is a weekday 😣");
+            case "Thursday "-> System.out.println("It is a weekday 😣");
+            case "Friday"-> System.out.println("It is a weekday 😣");
+            case "Saturday "-> System.out.println("It is a weekday 😣");
+            case "Sunday"-> System.out.println("It is a weekend 🥰");
+            default -> System.out.println(day + " is not a day ");
+        }
+    }
+
+}
