115cbddec0ea3a0902057a990655d6babf3f1dad
dhanuuuu7 <dhanusuji47@gmail.com>
2026-06-22 22:53:32 +0530

    Add prompt for user input on day

--- WorkTime.java
+++ WorkTime.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class WorkTime{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the day followed by Capital letter ");
         String day =  sc.nextLine();
 
 //        if(day.equals("Monday")){
