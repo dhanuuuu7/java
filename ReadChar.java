9fe299a4d1c980b6f0813c1dd5246fd6e852e603
dhanuuuu7 <dhanusuji47@gmail.com>
2026-07-15 17:06:31 +0530

    Add ReadChar class to read and display a character

---
+++ ReadChar.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class ReadChar {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a Character :");
+        char ch = sc.next().charAt(1);
+        System.out.println("Character is :" + ch);
+
+
+    }
+
+}
