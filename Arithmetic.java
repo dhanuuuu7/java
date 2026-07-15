b63ea8092d03677bf50be56e9baa50b23e981214
dhanuuuu7 <dhanusuji47@gmail.com>
2026-07-15 17:18:21 +0530

    Create Arithmetic.java

---
+++ Arithmetic.java
@@ -0,0 +1,14 @@
+public class Arithmetic {
+    public static void main(String [] args){
+        int p = 20 , q=5;
+
+        System.out.println(p+q);
+        System.out.println(p-q);
+        System.out.println(p*q);
+        System.out.println(p/q);
+        System.out.println(p%q);
+        System.out.println(p++);//first print then increment
+        System.out.println(p--);
+        System.out.println(++q);//first increment then print.
+    }
+}
