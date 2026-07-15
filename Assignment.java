7f369c080f447cf5dfc45eb08d5eb9cae9809979
dhanuuuu7 <dhanusuji47@gmail.com>
2026-07-15 17:33:09 +0530

    Create Assignment.java

---
+++ Assignment.java
@@ -0,0 +1,12 @@
+public class Assignment {
+    public static void main(String[] args){
+        int p = 20 , q = 10, r;
+
+        System.out.println(r=q);
+        System.out.println(p+=q);
+        System.out.println(p-=q);
+        System.out.println(p/=q);
+        System.out.println(p*=q);
+    }
+
+}
