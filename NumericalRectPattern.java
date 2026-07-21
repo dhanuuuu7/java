0576eca4268bac42a53331685e20309c596d8cde
dhanuuuu7 <dhanusuji47@gmail.com>
2026-07-21 17:11:24 +0530

    Create NumericalRectPattern.java

---
+++ NumericalRectPattern.java
@@ -0,0 +1,17 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class NumericalRectPattern {
+    public static void main (String [] args){
+        Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt();
+        for(int i = 1; i<=r; i++){
+            for(int j =i;j<=r;j++){
+                System.out.print(j);
+            }
+            for(int k =1 ; k<i ; k++) {
+                System.out.print(k);
+            }
+            System.out.println();
+        }
+    }
+}
